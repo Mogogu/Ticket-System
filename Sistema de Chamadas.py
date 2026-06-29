93cabb3a070aa6a85b3962b9039ee463e46b74da
Mogo Gu <shiroisenshiy@gmail.com>
2026-06-29 16:56:30 -0300

    Versão 1.0 - Sistema de Chamadas

---
+++ Sistema de Chamadas.py
@@ -0,0 +1,158 @@
+#Menu
+chamados = []
+alternativa = 0
+while alternativa != 5:
+    print('-' * 60)
+    print(' | Olá, você está no sistema de ajuda. Como posso ajudar? | ')
+    print('-' * 60)
+    print(' | 1 - Novo Chamado    | ')
+    print(' | 2 - Listar Chamados | ')
+    print(' | 3 - Buscar Chamado  | ')
+    print(' | 4 - Alterar Status  | ')
+    print(' | 5 - Sair            | ')
+    print('-' * 60)
+    alternativa1 = int(input(' | Para prosseguir informe qual é sua atual necessidade.  | R: '))
+    alternativa = alternativa1
+    print('-' * 60)
+    if alternativa == 1:
+        print(' | Você escolheu a opção [1] - Chamado | ')
+        print(' | Insira os dados a seguir.           | ')
+        print('-' * 60)
+        nome = str(input(' Nome :'))
+        setor = 0
+        while setor != 1 and setor != 2 and setor != 3 and setor != 4 and setor != 5 and setor != 6 and setor != 7 and setor != 8:
+            print(' Setores: ')
+            print(' | 1 - Financeiro  | ')
+            print(' | 2 - RH          | ')
+            print(' | 3 - Comercial   | ')
+            print(' | 4 - Marketing   | ')
+            print(' | 5 - Atendimento | ')
+            print(' | 6 - TI          | ')
+            print(' | 7 - Logística   | ')
+            print(' | 8 - Diretoria   | ')
+            setor = int(input('  Setor: '))
+            if setor == 1:
+                print(' | Você escolheu o Setor: Financeiro | ')
+                sete = 'Financeiro'
+            elif setor == 2:
+                print(' | Você escolheu o Setor: RH | ')
+                sete = 'RH'
+            elif setor == 3:
+                print(' | Você escolheu o Setor: Comercial | ')
+                sete = 'Comercial'
+            elif setor == 4:
+                print(' | Você escolheu o Setor: Marketing | ')
+                sete = 'Marketing'
+            elif setor == 5:
+                print(' | Você escolheu o Setor: Atendimento | ')
+                sete = 'Atendimento'
+            elif setor == 6:
+                print(' | Você escolheu o Setor: TI | ')
+                sete = 'TI'
+            elif setor == 7:
+                print(' | Você escolheu o Setor: Logística | ')
+                sete = 'Logística'
+            elif setor == 8:
+                print(' | Você escolheu o Setor: Diretoria | ')
+                sete = 'Diretoria'
+            else:
+                print(' | A Escolha não existe! Escolha dentro das opções para prosseguir. | ')
+        print(' Categorias: ')
+        print(' | 1 - Hardware         | ')
+        print(' | 2 - Windows          | ')
+        print(' | 3 - Linux            | ')
+        print(' | 4 - Microsoft Office | ')
+        print(' | 5 - Rede / Internet  | ')
+        print(' | 6 - Impressora       | ')
+        print(' | 7 - E-mail / Outlook | ')
+        print(' | 8 - Sistema Interno  | ')
+        print(' | 9 - Senhas           | ')
+        print(' | 10 - Outro           | ')
+        categoria = 0
+        while categoria != 1 and categoria != 2 and categoria != 3 and categoria != 4 and categoria != 5 and categoria != 6 and categoria != 7 and categoria != 8 and categoria != 9 and categoria != 10:
+            categoria = int(input('  Categoria: '))
+            if categoria == 1:
+                print(' | Você escolheu o Categoria: Hardware | ')
+                categ = 'Hardware'
+                prioridade = 'Alta'
+            elif categoria == 2:
+                print(' | Você escolheu o Categoria: Windows | ')
+                categ = 'Windows'
+                prioridade = 'Média'
+            elif categoria == 3:
+                print(' | Você escolheu o Categoria: Linux | ')
+                categ = 'Linux'
+                prioridade = 'Alta'
+            elif categoria == 4:
+                print(' | Você escolheu o Categoria: Microsoft Office | ')
+                categ = 'Microsoft Office'
+                prioridade = 'Média'
+            elif categoria == 5:
+                print(' | Você escolheu o Categoria: Rede / Internet | ')
+                categ = 'Rede / Internet'
+                prioridade = 'Alta'
+            elif categoria == 6:
+                print(' | Você escolheu o Categoria: Impressora | ')
+                categ = 'Impressora'
+                prioridade = 'Baixa'
+            elif categoria == 7:
+                print(' | Você escolheu o Categoria: E-mail / Outlook | ')
+                categ = 'E-mail / Outlook'
+                prioridade = 'Média'
+            elif categoria == 8:
+                print(' | Você escolheu o Categoria: Sistema Interno | ')
+                categ = 'Sistema Interno'
+                prioridade = 'Média'
+            elif categoria == 9:
+                print(' | Você escolheu o Categoria: Senhas | ')
+                categ = 'Senhas'
+                prioridade = 'Baixa'
+            elif categoria == 10:
+                print(' | Você escolheu o Categoria: Outro | ')
+                categ = 'Outro'
+                prioridade = 'Baixa'
+            else:
+                print(' | A Escolha não existe! Escolha dentro das opções para prosseguir. | ')
+        descricao = str(input(' Descrição: '))
+        print('-' * 60)
+        status = 'Aberto'
+        chamado = {'nome':nome, 'setor':sete, 'categoria':categ, 'descricao':descricao, 'prioridade':prioridade, 'status':status}
+        chamados.append(chamado)
+    elif alternativa == 2:
+        print(' | Você escolheu a opção [2] - Listar Chamados | ')
+        for chamado in chamados:
+            print(' | ', chamado ['nome'])
+    elif alternativa == 3:
+        print(' | Você escolheu a opção [3] - Buscar Chamados           | ')
+        print(' | Insira o dado a seguir para concluir a ação           | ')
+        print('-' * 60)
+        busca = str(input(' Nome :'))
+        for chamado in chamados:
+            if busca == chamado['nome']:
+                print(' | Nome:', chamado['nome'])
+                print(' | Setor:', chamado['setor'])
+                print(' | Categoria:', chamado['categoria'])
+                print(' | Descrição:', chamado['descricao'])
+                print(' | Prioridade:', chamado['prioridade'])
+                print(' | Status:', chamado['status'])
+    elif alternativa == 4:
+        print(' | Você escolheu a opção [4] - Alterar Status            | ')
+        print(' | Insira o dado a seguir para concluir a ação           | ')
+        print('-' * 60)
+        busca = str(input(' Nome :'))
+        for chamado in chamados:
+            if busca == chamado['nome']:
+                print('   Escolha um dos estados do chamado:   ')
+                print(' | [1] - Concluído    | ')
+                print(' | [2] - Em andamento | ')
+                altera = int(input(' R: '))
+                if altera == 1:
+                    chamado['status'] = 'Concluído'
+                    print('O novo status do chamado agora é "Concluído"')
+                elif altera == 2:
+                    chamado['status'] = 'Em andamento'
+                    print('O novo status do chamado agora é "Em andamento"')
+    elif alternativa == 5:
+        print('O programa será encerrado!')
+    else:
+        print('A Alternativa escolhida é inexistente! Caso deseje encerrar o programa, escolha a alternativa [5] ')
